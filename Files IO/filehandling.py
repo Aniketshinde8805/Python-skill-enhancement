beae49073c75a9bd1e5c80410739e2f74fa69db6
Aniket <aniket.shinde@ecolibriumenergy.com>
2022-01-21 10:06:52 +0530

    File handling practise

---
+++ Files IO/filehandling.py
@@ -0,0 +1,57 @@
+#open 
+file=open('file1.txt','r')  #here file is variable
+
+
+for i in file:
+    print(i)
+
+
+#for closing the file
+file.close() 
+
+
+file=open('file1.txt','r')
+
+print(file.read(4))
+
+file.close()
+
+
+#write mode w
+file=open('file2.txt','w')
+file.write('hi')
+
+file.write("hello")
+file.close()
+
+
+file=open('file2.txt','r')
+print(file.read())
+file.close()
+
+file=open('file1.txt','w')
+
+
+file.write("Ecolibrium energy")
+file=open('file1.txt','r')
+print(file.read())
+
+file.close()
+
+
+#append mode a
+file=open('file1.txt','a')
+file.write("pune")
+file=open('file1.txt','r')
+print(file.read())
+file=open('file1.txt','r+')
+file.write('aniket')
+print(file.read())
+
+file.tell()  #cursor position
+
+file.seek(0)
+
+print(file.read())
+
+file.close()
